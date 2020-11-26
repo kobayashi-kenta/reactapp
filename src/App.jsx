e980882aecce634b895ad0630372bd02001473ae
kobayashi-kenta <72721542+kobayashi-kenta@users.noreply.github.com>
2020-11-26 20:48:15 +0900

    11/26 practice

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { ColofulMessage } from "./components/ColofulMessage";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <ColofulMessage color="blue">お元気ですか？</ColofulMessage>
+      <ColofulMessage color="pink">元気です！</ColofulMessage>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>^ ^</p>}
+    </>
+  );
+};
+
+export default App;
